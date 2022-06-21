ce7fb53976803c56c0d4ac2f5c11b05483a3eab2
Inna <idinna91@mail.ru>
2022-06-21 21:00:39 +0300

    Global meanings

---
+++ pages/global_meaning.py
@@ -0,0 +1,40 @@
+class GlobalMeaning:
+    user_name = 'Roman'
+    password = 'Jdi1234'
+    NAME_USER = 'ROMAN IOVLEV'
+    PROPER_TEXT_1 = 'HOME'
+    PROPER_TEXT_2 = 'CONTACT FORM'
+    PROPER_TEXT_3 = 'SERVICE'
+    PROPER_TEXT_4 = 'METALS & COLORS'
+
+    TEXT_UNDER_PICT_1 = 'To include good practices and ideas from successful EPAM project'
+    TEXT_UNDER_PICT_2 = 'To be flexible and customizable'
+    TEXT_UNDER_PICT_3 = 'To be multiplatform'
+    TEXT_UNDER_PICT_4 = 'Already have good base (about 20 internal and some external projects), wish to get more…'
+
+    TEXT_ON_MAIN_PAGE_1 = 'EPAM FRAMEWORK WISHES…'
+    TEXT_ON_MAIN_PAGE_2 = 'LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, ' \
+                          'SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. ' \
+                          'UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO ' \
+                          'LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN REPREHENDERIT ' \
+                          'IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.'
+
+    TEXT_OF_SUB_HEADER = 'JDI GITHUB'
+
+    LINK = 'https://github.com/epam/JDI'
+
+    NUMBERS_OF_CHECKBOXES = 4
+    NUMBERS_OF_RADIOS = 4
+    NUMBERS_OF_DROP_DOWN = 1
+    NUMBERS_OF_BUTTONS = 2
+
+    SELEN = 'Selen'
+    WATER = 'Water'
+    WIND = 'Wind'
+    GOLD = 'Gold'
+    YELLOW = 'Yellow'
+    NUMBER_5 = '5'
+    NUMBER_8 = '8'
+    SUMMA = '13'
+
+    START_LOG_ROW_INDEX = 2

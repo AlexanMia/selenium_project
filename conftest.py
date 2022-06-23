3f21af1acb6a88f6b46da460d0272a57ece0e557
Inna <idinna91@mail.ru>
2022-06-23 16:11:47 +0300

    update

--- conftest.py
+++ conftest.py
@@ -1,14 +1,11 @@
-#работа с несколькими языками
-
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="class")
-def browser(request):
-    print("\nstart chrome browser for test..")
+def browser():
+    #print("\nstart chrome browser for test..")
     browser = webdriver.Chrome()
     yield browser
-    print("\nquit browser..")
+    #print("\nquit browser..")
     browser.quit()

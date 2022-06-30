5dfef4de9a668c0332961687e7fad9da4e9f1c6d
Inna <idinna91@mail.ru>
2022-06-30 15:23:33 +0300

    update locators

--- util/locators.py
+++ util/locators.py
@@ -1,10 +1,8 @@
-# файл для размещения селекторов, чтобы быстро исправить в одном месте
-# locators.py - тут мы храним локаторы, в виде констант. Локаторы каждой отдельной страницы завёрнуты в класс, чтобы было удобно импортировать
-
 from selenium.webdriver.common.by import By
-from .global_meaning import GlobalMeaning
+from util.constants import Constants
 
-class BasePageLocators():
+
+class Locators:
     LOGIN = (By.CSS_SELECTOR, "#user-icon")
     LOG = (By.CSS_SELECTOR, "#name")
     PASSWORD = (By.CSS_SELECTOR, "#password")
@@ -22,14 +20,14 @@ class BasePageLocators():
     IMAGE_3 = (By.CLASS_NAME, "icon-multi")
     IMAGE_4 = (By.CLASS_NAME, "icon-base")
 
-    TEXT_1 = (By.CLASS_NAME, "benefit-txt")
-    TEXT_2 = (By.XPATH, '//span[@class="benefit-txt" and contains(text(),"flexible")]')
-    TEXT_3 = (By.XPATH, '//span[@class="benefit-txt" and contains(text(),"multiplatform")]')
-    TEXT_4 = (By.XPATH, '//span[@class="benefit-txt" and contains(text(),"Already")]')
+    TEXT_INCLUDE = (By.CLASS_NAME, "benefit-txt")
+    TEXTS_PATTERN = '//span[@class="benefit-txt" and contains(text(),"{}")]'
+    TEXT_FLEXIBLE = (By.XPATH, TEXTS_PATTERN.format("flexible"))
+    TEXT_MULTIPLATFORM = (By.XPATH, TEXTS_PATTERN.format("multiplatform"))
+    TEXT_ALREADY = (By.XPATH, TEXTS_PATTERN.format("Already"))
 
     TEXT_of_the_MAIN_PAGE_1 = (By.CLASS_NAME, "main-title")
     TEXT_of_the_MAIN_PAGE_2 = (By.CLASS_NAME, "main-txt")
-
 
     TEXT_OF_SUB = (By.XPATH, '//a[@ui="link" and contains(text(),"JDI")]')
 
@@ -37,20 +35,22 @@ class BasePageLocators():
 
     FOOTER = (By.CLASS_NAME, "footer-bg")
 
-    SUPPORT_UNDER_SERVICE = (By.XPATH, '//a[text()="Support"]')
-    DATES_UNDER_SERVICE = (By.XPATH, '//a[text()="Dates"]')
-    COMPLEX_TABLE_UNDER_SERVICE = (By.XPATH, '//a[text()="Complex Table "]')
-    SIMPLE_TABLE_UNDER_SERVICE = (By.XPATH, '//a[text()="Simple Table "]')
-    TABLE_WITH_PAGES_UNDER_SERVICE = (By.XPATH, '//a[text()="Table with pages"]')
-    DIFFERENT_ELEMENTS_UNDER_SERVICE = (By.XPATH, '//a[text()="Different elements"]')
+    UNDER_SERVICE_BAR_PATTERN = '//a[text()="{}"]'
+    SUPPORT_UNDER_SERVICE = (By.XPATH, UNDER_SERVICE_BAR_PATTERN.format("Support"))
+    DATES_UNDER_SERVICE = (By.XPATH, UNDER_SERVICE_BAR_PATTERN.format("Dates"))
+    COMPLEX_TABLE_UNDER_SERVICE = (By.XPATH, UNDER_SERVICE_BAR_PATTERN.format("Complex Table "))
+    SIMPLE_TABLE_UNDER_SERVICE = (By.XPATH, UNDER_SERVICE_BAR_PATTERN.format("Simple Table "))
+    TABLE_WITH_PAGES_UNDER_SERVICE = (By.XPATH, UNDER_SERVICE_BAR_PATTERN.format("Table with pages"))
+    DIFFERENT_ELEMENTS_UNDER_SERVICE = (By.XPATH, UNDER_SERVICE_BAR_PATTERN.format("Different elements"))
 
-    SERVICE_ON_THE_LEFT = (By.XPATH, '//span[contains(text(),"Service")]')
-    SUPPORT_SERVICE_ON_THE_LEFT = (By.XPATH, '//span[contains(text(),"Support")]')
-    DATES_SERVICE_ON_THE_LEFT = (By.XPATH, '//span[contains(text(),"Dates")]')
-    COMPLEX_TABLE_SERVICE_ON_THE_LEFT = (By.XPATH, '//span[contains(text(),"Complex Table ")]')
-    SIMPLE_TABLE_SERVICE_ON_THE_LEFT = (By.XPATH, '//span[contains(text(),"Simple Table")]')
-    TABLE_WITH_PAGES_SERVICE_ON_THE_LEFT = (By.XPATH, '//span[contains(text(), "Table with pages")]')
-    DIFFERENT_ELEMENTS_SERVICE_ON_THE_LEFT = (By.XPATH, '//span[contains(text(), "Different elements")]')
+    LEFT_SIDE_BAR_LOCATOR_PATTERN = '//span[contains(text(),"{}")]'
+    SERVICE_ON_THE_LEFT = (By.XPATH, LEFT_SIDE_BAR_LOCATOR_PATTERN.format("Service"))
+    SUPPORT_SERVICE_ON_THE_LEFT = (By.XPATH, LEFT_SIDE_BAR_LOCATOR_PATTERN.format("Support"))
+    DATES_SERVICE_ON_THE_LEFT = (By.XPATH, LEFT_SIDE_BAR_LOCATOR_PATTERN.format("Dates"))
+    COMPLEX_TABLE_SERVICE_ON_THE_LEFT = (By.XPATH, LEFT_SIDE_BAR_LOCATOR_PATTERN.format("Complex Table "))
+    SIMPLE_TABLE_SERVICE_ON_THE_LEFT = (By.XPATH, LEFT_SIDE_BAR_LOCATOR_PATTERN.format("Simple Table"))
+    TABLE_WITH_PAGES_SERVICE_ON_THE_LEFT = (By.XPATH, LEFT_SIDE_BAR_LOCATOR_PATTERN.format("Table with pages"))
+    DIFFERENT_ELEMENTS_SERVICE_ON_THE_LEFT = (By.XPATH, LEFT_SIDE_BAR_LOCATOR_PATTERN.format("Different elements"))
 
     IFRAME = (By.CSS_SELECTOR, "#frame")
     IFRAME_BUTTON = (By.CSS_SELECTOR, "#frame-button")
@@ -67,7 +67,7 @@ class DifferentElements:
     LEFT_SECTION = (By.CSS_SELECTOR, "#mCSB_1")
 
     CHECKBOX_WATER = (By.XPATH, '//label[@class="label-checkbox"][1]//input')
-    CHECKBOX_WATER_sel = (By.XPATH, '//label[@class="label-checkbox"][1]//input[@type="checkbox"]')
+    SELECTED_CHECKBOX_WATER = (By.XPATH, '//label[@class="label-checkbox"][1]//input[@type="checkbox"]')
 
     CHECKBOX_WIND = (By.XPATH, '//label[@class="label-checkbox"][3]//input')
 
@@ -79,48 +79,44 @@ class DifferentElements:
 
 
 class MetalsColors:
-
     metalscolours = (By.XPATH, '//span[contains(text(),"Metals & Colors")]')
-    RADIO_5 = (By.XPATH, f'//p[@class="radio"]//label[text()={GlobalMeaning.NUMBER_5}]')
-    RADIO_5_sel = (By.XPATH, '//p[@class="radio"]//input[@id="p3"]')
+    RADIO_5 = (By.XPATH, f'//p[@class="radio"]//label[text()={Constants.NUMBER_5}]')
+    SELECTED_RADIO_5 = (By.XPATH, '//p[@class="radio"]//input[@id="p3"]')
 
-    RADIO_8 = (By.XPATH, f'//p[@class="radio"]//label[text()={GlobalMeaning.NUMBER_8}]')
-    RADIO_8_sel = (By.XPATH, '//p[@class="radio"]//input[@id="p8"]')
+    RADIO_8 = (By.XPATH, f'//p[@class="radio"]//label[text()={Constants.NUMBER_8}]')
+    SELECTED_RADIO_8 = (By.XPATH, '//p[@class="radio"]//input[@id="p8"]')
 
     CALCULATE_BUTTON = (By.XPATH, '//button[@id="calculate-button"]')
 
     CALCULATE_BUTTON_RESULTS = (By.XPATH, '//li[@class="summ-res"]')
 
-    CHECKBOX_WATER = (By.XPATH, f'//p[@class="checkbox"]//label[text()="{GlobalMeaning.WATER}"]')
-    CHECKBOX_WATER_sel = (By.XPATH, '//p[@class="checkbox"]//input[@id="g1"]')
+    CHECKBOX_WATER = (By.XPATH, f'//p[@class="checkbox"]//label[text()="{Constants.WATER}"]')
+    SELECTED_CHECKBOX_WATER = (By.XPATH, '//p[@class="checkbox"]//input[@id="g1"]')
 
-    CHECKBOX_WIND = (By.XPATH, f'//p[@class="checkbox"]//label[text()="{GlobalMeaning.WIND}"]')
-    CHECKBOX_WIND_sel = (By.XPATH, '//p[@class="checkbox"]//input[@id="g3"]')
+    CHECKBOX_WIND = (By.XPATH, f'//p[@class="checkbox"]//label[text()="{Constants.WIND}"]')
+    SELECTED_CHECKBOX_WIND = (By.XPATH, '//p[@class="checkbox"]//input[@id="g3"]')
 
     LOGS = (By.XPATH, '//ul[@class="panel-body-list logs"]/child::li')
 
     DROP_DOWN_BUTTON_COLORS = (By.XPATH, '//button[@title="Colors"]')
     DROP_DOWN_YELLOW = (By.XPATH, '//li[@rel="4"]')
-    DROP_DOWN_YELLOW_SEL = (By.XPATH, '//li[@class="selected"]')
+    SELECTED_DROP_DOWN_YELLOW = (By.XPATH, '//li[@class="selected"]')
 
     DROP_DOWN_BUTTON_METALS = (By.XPATH, '//button[@title="Metals"]//span[@class="caret"]')
-    DROP_DOWN_GOLD = (By.XPATH, f'//span[@class="text" and text()="{GlobalMeaning.GOLD}"]')
-    DROP_DOWN_GOLD_SEL = (By.XPATH, f'//span[text()="{GlobalMeaning.GOLD}"]')
+    DROP_DOWN_GOLD = (By.XPATH, f'//span[@class="text" and text()="{Constants.GOLD}"]')
+    SELECTED_DROP_DOWN_GOLD = (By.XPATH, f'//span[text()="{Constants.GOLD}"]')
 
     DROP_DOWN_BUTTON_VEGETABLES = (By.XPATH, '//div[@id="salad-dropdown"]')
     DROP_DOWN_VEGETABLES = (By.XPATH, '//label[@for="g7"]')
     DROP_DOWN_CUCUMBER = (By.XPATH, '//label[@for="g5"]')
-    DROP_DOWN_CUCUMBER_SEL = (By.XPATH, '//input[@id="g5"]')
+    SELECTED_DROP_DOWN_CUCUMBER = (By.XPATH, '//input[@id="g5"]')
     DROP_DOWN_TOMATO = (By.XPATH, '//label[@for="g6"]')
-    DROP_DOWN_TOMATO_SEL = (By.XPATH, '//input[@id="g6"]')
+    SELECTED_DROP_DOWN_TOMATO = (By.XPATH, '//input[@id="g6"]')
 
     SUBMIT_BUTTON = (By.XPATH, '//button[@id="submit-button"]')
 
     LOG_ROW = '//ul[@class="panel-body-list results"]//li[{}]'
-    RESULTS_ELEM = (By.XPATH, '//ul[@class="panel-body-list results"]//li[2]')
-    RESULTS_COL = (By.XPATH, '//ul[@class="panel-body-list results"]//li[3]')
-    RESULTS_MET = (By.XPATH, '//ul[@class="panel-body-list results"]//li[4]')
-    RESULTS_SAL = (By.XPATH, '//ul[@class="panel-body-list results"]//li[5]')
+
 
 
 

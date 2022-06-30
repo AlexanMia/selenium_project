e3ec6643d3649b1d934bcd7b710322e8bf156888
Inna <idinna91@mail.ru>
2022-06-30 15:24:57 +0300

    update test_1

--- test_1.py
+++ test_1.py
@@ -1,57 +1,54 @@
-import pytest
-from .pages.locators import BasePageLocators
-from .pages.global_meaning import GlobalMeaning
+from .util.locators import Locators
+from .util.constants import Constants
 from .pages.base_page import BasePage
 
 
 class Test1:
     def test_open_test_site(self, browser):
         global page
-        link = "https://jdi-testing.github.io/jdi-light/index.html"
-        page = BasePage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page = BasePage(browser, Constants.link_1)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()  # открываем страницу
-
 
     def test_proper_title(self):
         # POINT 2
         global page
-        assert page.should_be_definite_title() == "Home Page", "Title is not Home Page"
+        assert page.get_page_title() == Constants.PAGE_TITLE, f"Title is not {Constants.PAGE_TITLE}"
 
     def test_logging(self, browser):
         # CHECKED
         # POINT 3
         # LOG IN
         global page
-        page.element_click(BasePageLocators.LOGIN)
-        page.enter_value_into_box(BasePageLocators.LOG, GlobalMeaning.user_name)
-        page.enter_value_into_box(BasePageLocators.PASSWORD, GlobalMeaning.password)
-        page.element_click(BasePageLocators.BUTTON_ENTER)
+        page.element_click(Locators.LOGIN)
+        page.enter_value_into_box(Locators.LOG, Constants.user_name)
+        page.enter_value_into_box(Locators.PASSWORD, Constants.password)
+        page.element_click(Locators.BUTTON_ENTER)
 
     def test_proper_name_user(self, browser):
         # POINT 4
         global page
-        assert page.find_element_1(BasePageLocators.NAME_USER).text == GlobalMeaning.NAME_USER, \
-            f"User's name is not {GlobalMeaning.NAME_USER}"
+        assert page.get_elements_text(Locators.NAME_USER) == Constants.NAME_USER, \
+            f"User's name is not {Constants.NAME_USER}"
 
     def test_proper_title(self, browser):
         # CHECKED
         # POINT 5
         global page
-        assert page.should_be_definite_title() == "Home Page", "Title is not Home Page"
-
+        # TODO одна и та же функция дважды, это ничего?
+        assert page.get_page_title() == Constants.PAGE_TITLE, f"Title is not {Constants.PAGE_TITLE}"
 
     def test_is_items_and_propers_text(self, browser):
         # CHECKED
         # POINT 6
-        items_locators = [BasePageLocators.home,
-                          BasePageLocators.contact_form,
-                          BasePageLocators.service,
-                          BasePageLocators.metalscolours
+        items_locators = [Locators.home,
+                          Locators.contact_form,
+                          Locators.service,
+                          Locators.metalscolours
                           ]
-        expected_items_of_main_page = [GlobalMeaning.PROPER_TEXT_1,
-                                       GlobalMeaning.PROPER_TEXT_2,
-                                       GlobalMeaning.PROPER_TEXT_3,
-                                       GlobalMeaning.PROPER_TEXT_4
+        expected_items_of_main_page = [Constants.PROPER_TEXT_1,
+                                       Constants.PROPER_TEXT_2,
+                                       Constants.PROPER_TEXT_3,
+                                       Constants.PROPER_TEXT_4
                                        ]
         actual_texts_items = []
         [actual_texts_items.append(page.get_elements_text(i)) for i in items_locators]
@@ -61,21 +58,21 @@ class Test1:
         # CHECKED
         # POINT 7
         global page
-        images_locators = [BasePageLocators.IMAGE_1,
-                           BasePageLocators.IMAGE_2,
-                           BasePageLocators.IMAGE_3,
-                           BasePageLocators.IMAGE_4]
-        assert all([page.search_element_m(i) for i in images_locators]), 'Not all elements are showed'
+        images_locators = [Locators.IMAGE_1,
+                           Locators.IMAGE_2,
+                           Locators.IMAGE_3,
+                           Locators.IMAGE_4]
+        assert all([page.find_need_element(i) for i in images_locators]), 'Not all elements are showed'
 
     def test_are_four_texts_under_images(self, browser):
         # CHECKED
         # POINT 8
         global page
-        texts_locators = [BasePageLocators.TEXT_1, BasePageLocators.TEXT_2, BasePageLocators.TEXT_3,
-                          BasePageLocators.TEXT_4]
-        expected_texts = [GlobalMeaning.TEXT_UNDER_PICT_1, GlobalMeaning.TEXT_UNDER_PICT_2,
-                          GlobalMeaning.TEXT_UNDER_PICT_3,
-                          GlobalMeaning.TEXT_UNDER_PICT_4]
+        texts_locators = [Locators.TEXT_INCLUDE, Locators.TEXT_FLEXIBLE, Locators.TEXT_MULTIPLATFORM,
+                          Locators.TEXT_ALREADY]
+        expected_texts = [Constants.TEXT_UNDER_PICT_1, Constants.TEXT_UNDER_PICT_2,
+                          Constants.TEXT_UNDER_PICT_3,
+                          Constants.TEXT_UNDER_PICT_4]
         actual_texts = []
         [actual_texts.append(page.get_elements_text(i).replace('\n', ' ')) for i in texts_locators]
         assert all([a == b for a, b in zip(actual_texts, expected_texts)]), 'Texts are not equal'
@@ -85,8 +82,8 @@ class Test1:
         # POINT 9
         global page
         texts_of_main_page = {
-            page.search_element_m(BasePageLocators.TEXT_of_the_MAIN_PAGE_1).text: GlobalMeaning.TEXT_ON_MAIN_PAGE_1,
-            page.search_element_m(BasePageLocators.TEXT_of_the_MAIN_PAGE_2).text: GlobalMeaning.TEXT_ON_MAIN_PAGE_2
+            page.get_elements_text(Locators.TEXT_of_the_MAIN_PAGE_1): Constants.TEXT_ON_MAIN_PAGE_1,
+            page.get_elements_text(Locators.TEXT_of_the_MAIN_PAGE_2): Constants.TEXT_ON_MAIN_PAGE_2
             }
         for i, j in texts_of_main_page.items():
             assert i == j, f'Texts is not equal {i} = {j}'
@@ -95,48 +92,48 @@ class Test1:
         # CHECKED
         # POINT 10
         global page
-        assert page.search_element_m(BasePageLocators.IFRAME), 'Iframe is not be found'
+        assert page.find_need_element(Locators.IFRAME), 'Iframe is not be found'
 
     def test_switch_to_iframe(self, browser):
         # CHECKED
         # POINT 11
         global page
         browser.switch_to.frame('frame')
-        assert page.search_element_m(BasePageLocators.IFRAME_BUTTON), 'Iframe button is not be found'
+        assert page.find_need_element(Locators.IFRAME_BUTTON), 'Iframe button is not be found'
 
     def test_switch_to_original_window(self, browser):
         # CHECKED
         # POINT 12
         global page
         browser.switch_to.default_content()
-        assert page.search_element_m(BasePageLocators.TEXT_OF_SUB).text == GlobalMeaning.TEXT_OF_SUB_HEADER, \
+        assert page.get_elements_text(Locators.TEXT_OF_SUB) == Constants.TEXT_OF_SUB_HEADER, \
             'Exit from frame is unsuccessful'
 
     def test_proper_text_of_the_header(self, browser):
         # CHECKED
         # POINT 13
         global page
-        assert page.search_element_m(BasePageLocators.TEXT_OF_SUB).text == GlobalMeaning.TEXT_OF_SUB_HEADER, \
+        assert page.get_elements_text(Locators.TEXT_OF_SUB) == Constants.TEXT_OF_SUB_HEADER, \
             'Texts are not equal'
 
     def test_proper_link(self, browser):
         # CHECKED
         # POINT 14
         global page
-        assert page.search_element_m(BasePageLocators.TEXT_OF_SUB).get_attribute('href') == GlobalMeaning.LINK, \
+        assert page.find_need_element(Locators.TEXT_OF_SUB).get_attribute('href') == Constants.LINK, \
             'Links are not equal'
 
     def test_is_left_section(self, browser):
         # CHECKED
         # POINT 15
         global page
-        assert page.search_element_m(BasePageLocators.LEFT_SECTION), 'Left section is not displayed'
+        assert page.find_need_element(Locators.LEFT_SECTION), 'Left section is not displayed'
 
     def test_is_footer(self, browser):
         # CHECKED
         # POINT 16
         global page
-        assert page.search_element_m(BasePageLocators.FOOTER), 'Footer is not displayed'
+        assert page.find_need_element(Locators.FOOTER), 'Footer is not displayed'
 
 
 
